c7d683d40fd4ca0448d4a645ec3ee1efab7fd257
passmgrgui <135318604+passmgrgui@users.noreply.github.com>
2023-11-29 17:09:31 +1100

    created separate file for shortned links due to new domain for that speciicall

--- shortener/app.py
+++ shortener/app.py
@@ -20,16 +20,6 @@ def index():
     return render_template("index.html", urls=urls if urls else None)
 
 
-@app.route("/<path:shortened>")
-def _redirect(shortened):
-    destination_url = get_destination_url(shortened)
-    return (
-        redirect(f"http://{destination_url}")
-        if destination_url is not None
-        else abort(404)
-    )
-
-
 @app.route("/add", methods=["POST"])
 def add():
     url = request.json["url"]
--- shortener/wsgi.py
+++ shortener/wsgi.py
@@ -1,4 +1,6 @@
 from app import app
+from links import app as links
 
 if __name__ == "__main__":
     app.run()
+    links.run()
