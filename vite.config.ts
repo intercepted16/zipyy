308735ce86d0cf8dc1903b4e1bf5041e54b37e16
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-06-22 20:19:28 +1000

    fix: top-level-await not supported during build process

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,16 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000
+  },
+  build: {
+    target: "es2022"
+  },
+  esbuild: {
+    target: "es2022"
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2022"
+    }
   }
 });
